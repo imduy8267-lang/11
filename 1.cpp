d0a3a30f787705dc1db9bd657bcf5f2d15460c6c
Nhat Duy <Nhat Duy@DUY>
2026-09-09 18:35:02 +0700

    Add project files.

---
+++ 1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a, b;
+    cout << "Nhap so thu nhat: ";
+    cin >> a;
+    cout << "Nhap so thu hai: ";
+    cin >> b;
+
+    int sum = a + b;
+    cout << "Tong hai so la: " << sum << endl;
+
+    return 0;
+}
